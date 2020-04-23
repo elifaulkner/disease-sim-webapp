6cd5df5da36c1c7db8835109775ef9e7590135c0
Eli Faulkner <efaulkner@chathamfinancial.com>
2020-04-23 15:14:46 -0400

    Chart cleanup

--- src/IDMGraphHighcharts.js
+++ src/IDMGraphHighcharts.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import Highcharts from 'highcharts';
+import HighchartsExporting from 'highcharts/modules/exporting';
 
 const IDMGraphHighcharts = (props) => {
-    const chartRef = React.createRef();
+
+    HighchartsExporting(Highcharts)
 
     const makeGuid = () => {
         var s4 = () => (((1+Math.random())*0x10000)|0).toString(32)
@@ -18,12 +20,15 @@ const IDMGraphHighcharts = (props) => {
                 var data = [{'x':0, 'y':child.props.init_y}]
                 if(child.props.times != null) {
                     child.props.times.forEach((x, i) => {
-                        data.push({'x':child.props.times[i], 'y':child.props.values[i]})
+                        data.push({'x':child.props.times[i], 'y':Math.round(child.props.values[i]*1.0E6)/1.0E4})
                     });    
                 }
                 return {
                     name: child.props.name,
-                    data: data                
+                    data: data,
+                    type: 'line',
+                    color: child.props.color,
+                    showInLegend: false
                 }
             }))
     }, [props.children]);
@@ -34,7 +39,10 @@ const IDMGraphHighcharts = (props) => {
                 type: 'scatter'
             },
             title: {
-                text: props.title
+                text: props.title,
+                style: {
+                    fontSize: 32
+                }
             },
             xAxis: {
                 title: {
@@ -50,7 +58,7 @@ const IDMGraphHighcharts = (props) => {
             plotOptions: {
                 series: [{
                     // specific options for this series instance
-                    type: 'areaspline'
+                    type: 'line'
                 }],
                 scatter: {
                     marker: false,
@@ -59,9 +67,14 @@ const IDMGraphHighcharts = (props) => {
             },
             legend: {
                 enable: false
+            },
+            tooltip: {
+                useHTML: true,
+                shared: true,
+                pointFormat: '<span style="color:{point.color}">●</span> {point.y}% {series.name} on day {point.x}<br/>'
             }
         });
-    }, [curves, props.title, chartRef, guid])
+    }, [curves, props.title, guid])
     useEffect(() => {
         build_child_data_sets();
     }, [props.children, build_child_data_sets])
