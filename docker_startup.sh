0f1aa857ddb588a152cf07339512947e6355ef13
Eli Faulkner <efaulkner@chathamfinancial.com>
2020-05-20 21:33:59 -0400

    Updated the log location

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,4 +1,4 @@
 echo "Starting nginx"
 /usr/sbin/nginx
 echo "Starting API"
-conda run -n idm gunicorn --bind=0.0.0.0:5000 --access-logfile /var/log/gunicorn-access --log-file /var/log/gunicorn-error main:app
+conda run -n idm gunicorn --bind=0.0.0.0:5000 --access-logfile /var/log/gunicorn/gunicorn-access --log-file /var/log/gunicorn/gunicorn-error main:appY
