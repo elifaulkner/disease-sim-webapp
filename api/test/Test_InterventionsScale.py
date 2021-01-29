a8757c3b0bd97988455ba1f6de1612032bc9ee6c
Eli Faulkner <efaulkner@chathamfinancial.com>
2021-01-29 12:46:33 -0500

    I added immunization to the scir model

--- api/test/Test_InterventionsScale.py
+++ api/test/Test_InterventionsScale.py
@@ -6,6 +6,7 @@ class TestInterventionsScale(unittest.TestCase):
         self.interventions = scir.Interventions()
         self.interventions.infection_rate.append(scir.Intervention("test1", 1, 100, .5))
         self.interventions.infection_rate.append(scir.Intervention("test1", 50, 150, .5))
+        self.interventions.immunization_rate.append(scir.Intervention("test3", 50, 150, .05))
 
     def test_single_intervention(self):
         self.assertEqual(.5, self.interventions.get_infection_rate_scale(25))
@@ -15,3 +16,9 @@ class TestInterventionsScale(unittest.TestCase):
 
     def test_single_intervention2(self):
         self.assertEqual(.5, self.interventions.get_infection_rate_scale(125))
+
+    def test_immunization_rate(self):
+        self.assertEqual(0, self.interventions.get_immunization_rate(10))
+
+    def test_immunization_rate2(self):
+        self.assertEqual(0.05, self.interventions.get_immunization_rate(100))
